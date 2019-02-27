473eb084049fe7c988d6cf00421a64340be4728f
Jhon <Jhon@10.203.65.8>
2019-02-27 13:20:17 -0500

    izi

--- src/Figuracuerpo/Tortuga2.java
+++ src/Figuracuerpo/Tortuga2.java
@@ -20,36 +20,39 @@ public class Tortuga2 extends Thread{
         this.tortuga.left(20);
         this.tortuga.forward(5);
         this.tortuga.right(70);
-        this.tortuga.forward(20);
+        this.tortuga.forward(16);
         this.tortuga.left(50);
         this.tortuga.penDown();
         
         this.tortuga.right(135);
-        this.tortuga.forward(36.00684839);
+        this.tortuga.forward(36.00684839*(9.0/10.0));
         this.tortuga.right(135);
-        this.tortuga.forward(30);
+        this.tortuga.forward(30*(9.0/10.0));
         this.tortuga.right(100);
-        this.tortuga.forward(25.8965397); 
+        this.tortuga.forward(25.8965397*(9.0/10.0)); 
         this.tortuga.penUp();
         
         this.tortuga.left(100);
-        this.tortuga.forward(84);
+        this.tortuga.forward(80);
         this.tortuga.right(90);
         this.tortuga.penDown();
         this.tortuga.left(135);
-        this.tortuga.forward(36.00684839);
+        this.tortuga.forward(36.00684839*(9.0/10.0));
         this.tortuga.left(135);
-        this.tortuga.forward(30);
+        this.tortuga.forward(30*(9.0/10.0));
         this.tortuga.left(100);
-        this.tortuga.forward(25.8965397); 
+        this.tortuga.forward(25.8965397*(9.0/10.0)); 
         this.tortuga.penUp();
         this.tortuga.right(100);
         
         System.out.println("X: " + this.tortuga.getX() + "Y: " + this.tortuga.getY());
         
-        this.tortuga.forward(13.3892119);
+        this.tortuga.forward(12.4533896);
         this.tortuga.right(90);
-        this.tortuga.forward(17.6390681);
+        this.tortuga.forward(15.0594324);
+        this.tortuga.left(90);
+        this.tortuga.forward(3);
+        this.tortuga.right(90);
         this.tortuga.penDown();
         this.tortuga.forward(8);
         this.tortuga.left(90);
@@ -57,7 +60,7 @@ public class Tortuga2 extends Thread{
         this.tortuga.left(90);
         this.tortuga.forward(8);
         this.tortuga.right(90);
-        this.tortuga.forward(17);
+        this.tortuga.forward(11);
         this.tortuga.right(90);
         this.tortuga.forward(8);
         this.tortuga.left(90);
